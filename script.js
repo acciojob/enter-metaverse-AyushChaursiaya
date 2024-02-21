48bc8f4c6a0b7cfd099f6bfdc820d3315ff9859c
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-21 13:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+
+document.getElementById("enterBtn").addEventListener("cilck", function() {
+	document.getElementById("status").innerHTML = "<h1>Enter the Metaverse</h1>";
+});
