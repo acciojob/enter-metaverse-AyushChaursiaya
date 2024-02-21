8da77cca25dd63c38e9652764d4a0f68181ed6cf
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-21 13:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 document.getElementById("enterBtn").addEventListener("click", function() {
-	document.getElementById("status").innerHTML = "<h1>Enter the Metaverse</h1>";
+	document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
 });
 
 
